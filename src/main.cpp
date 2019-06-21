6062c1841f5f5a963c726906134caf606640dc26
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2019-06-21 13:57:43 +0900

    add double knock

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ Queue<float> mvQueue = Queue<float>(10);
 
 const int sPin = A0;
 int outpin = 8;
-int high_thresh = 3;
-int low_thresh = 1;
+int high_thresh = 30;
+int low_thresh = 5;
 float last_knock_time = 0.0;
 
 ///////////////
@@ -26,24 +26,16 @@ int read_sensor() {
     return sValue;
 }
 
-void write_time(int value, float time, int n) {
-    if(n != 1 && n != 2) return;
+void write_time(int value, float time) {
     float mvTime = time + 2.0;
     mvQueue.Push(mvTime);
     Serial.print("WriteTime : ");
     Serial.println(mvTime);
-    if(n == 2) {
-        mvTime += 0.8;
-        myQueue.Push(mvTime);
-        Serial.print("WriteTime : ");
-        Serial.println(mvTime);
-    }
 }
 
 void judge(){
     int s = read_sensor();
     float now = millis()/1000.0f;
-    float lastTime;
     if(s > max_knock) {
         max_knock = s;
     }
@@ -58,10 +50,12 @@ void judge(){
     else if (s < low_thresh && bPotential) {
         bPotential = false;
         Serial.print("Got Knock! ");
-        if(max_knock < 80)
-            write_time(s, now, 1);
-        else
-            write_time(s, now, 2);
+        if(max_knock < 100) {
+            write_time(s, now);
+        } else {
+            write_time(s, now);
+            write_time(s, now+0.8);
+        }
     }
 }
 
